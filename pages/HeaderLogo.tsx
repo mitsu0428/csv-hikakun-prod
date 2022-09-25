32cd21753abe0a5e45b4f6084e61836f90d4e13b
mitsu0428 <mitsuprograming@gmail.com>
2022-09-25 23:58:45 +0900

    fix(logo)

--- pages/HeaderLogo.tsx
+++ pages/HeaderLogo.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 function HeaderLogo() {
   return (
     <div>
-        <Image src="/images/csvhikakun_logo_ver0.1.png" width={75} height={75} alt="csvhikrakunのロゴ" />
+        <Image src="/images/csvhikakun_logo_ver0.2.png" width={75} height={75} alt="csvhikrakunのロゴ" />
     </div>
   )
 }
