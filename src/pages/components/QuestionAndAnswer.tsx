48d7bd3a0a408db174e176a73855987a40bc4298
mitsu0428 <mitsuprograming@gmail.com>
2022-10-02 03:26:34 +0900

    fix

--- src/pages/components/QuestionAndAnswer.tsx
+++ src/pages/components/QuestionAndAnswer.tsx
@@ -3,15 +3,13 @@ import styles from "../../styles/QuestionAndAnswer.module.css";
 
 type Props = {
   title: string;
-  withIcon?: boolean;
   children?: ReactNode;
 };
 
-export const Menu: FC<Props> = ({ title, withIcon, children }) => {
+export const Menu: FC<Props> = ({ title, children }) => {
   const childElement = useRef<HTMLDivElement>(null);
   const [showChildren, setshowChildren] = useState(false);
   const [childHeight, setChildHeight] = useState(0);
-  const [reverseIcon, setReverseIcon] = useState(false);
 
   useEffect(() => {
     if (childElement.current) {
@@ -24,31 +22,29 @@ export const Menu: FC<Props> = ({ title, withIcon, children }) => {
     if (childElement.current) {
       setshowChildren(!showChildren);
     }
-    setReverseIcon(!reverseIcon);
   };
 
   return (
-    <>
-      <div onClick={handleClick} className={styles.item}>
-        {title}
-        {withIcon && (
-          <div className={`${styles.icon} ${reverseIcon && styles.reverse}`}>
-            <img
-              style={{ height: 24, width: 24 }}
-              src="images/expand_more.svg"
-            />
-          </div>
-        )}
-      </div>
-      <div
-        className={styles.childItem}
-        style={{
-          height: children && showChildren ? `${childHeight}px` : "0px",
-          opacity: children && showChildren ? 1 : 0,
-        }}
-      >
-        <div ref={childElement}>{children}</div>
-      </div>
-    </>
+    <div className={styles.itemRoot}>
+        <div className={styles.itemChild}>
+            <h3 
+              onClick={handleClick} 
+              className={styles.item}>
+                {title}
+            </h3>
+            <div
+              className={styles.childItem}
+              style={{
+                height: children && showChildren ? `${childHeight}px` : "0px",
+                opacity: children && showChildren ? 1 : 0,
+              }}
+            >
+                <div
+                  className={styles.item}
+                  ref={childElement}>{children}
+                </div>
+            </div>
+        </div>
+    </div>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -219,14 +219,26 @@ const Home: NextPage = () => {
             <CSVDownloader data={csvCompareRowCol} className={styles.card}/>
           </div>
         </div>
-        <Menu title="Q & A" withIcon={true}>
+        <Menu title="参考情報">
           <Menu title="CSVとは??">
+              <p>
+              comma separated valuesファイルです。カンマによって値が分けられているものを指します。
+              </p>
           </Menu>
           <Menu title="CSVの行だけの差分を調べたい。">
+              <p>
+              比較したいCSVを二つ選択し、行だけを比較するを押下することで比較することができます。
+              </p>
           </Menu>
           <Menu title="CSVの値だけの差分を調べたい。">
+              <p>
+              比較したいCSVを二つ選択し、値だけを比較するを押下することで比較することができます。
+              </p>
           </Menu>
           <Menu title="比較したCSVの差分をダウンロードしたい。">
+              <p>
+              ファイルを選択した後に比較し、ダウンロードボタンを押すことでCSV出力が可能です。
+              </p>
           </Menu>
         </Menu>
       </main>
