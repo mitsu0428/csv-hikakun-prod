f7cd496a1f2d0401dc826a06df76da14e1268168
mitsu0428 <mitsuprograming@gmail.com>
2022-10-03 00:23:53 +0900

    fix(Q&A)

--- src/pages/components/Accordion.tsx
+++ src/pages/components/Accordion.tsx
@@ -1,68 +1,47 @@
 import React from 'react'
-import {useEffect} from "react";
 import styles from '../../styles/QuestionAndAnswer.module.css'
 
 function Accordion() {
-  useEffect(() => {
-    const triggers = document.querySelectorAll(".accordiontrigger");
-    triggers.forEach((trigger) => {
-      const controls: any = trigger.getAttribute("aria-controls");
-      const panel: any = document.getElementById(controls);
-  
-      trigger.addEventListener("click", (e) => {
-        const target: any = e.currentTarget;
-        const isOpen: Boolean = target.getAttribute("aria-expanded") === "true";
-  
-        if (isOpen) {
-          // アコーディオンを閉じる
-          target.setAttribute("aria-expanded", "false");
-          panel.classList.add("__close");
-        } else {
-          // アコーディオンを開く
-          target.setAttribute("aria-expanded", "true");
-          panel.classList.remove("__close");
-        }
-      });
-  }, []);
-});
   return (
-    <div>
-      <h1 className={styles.title}>
-        よくある質問
-      </h1>
-      <div className={styles.accordion}>
-        <h2 className={styles.accordionheader}>
-          <button className={styles.accordiontrigger} aria-expanded="false" aria-controls="accordion-panel-1">
-            CSVとは??<span className={styles.accordionicon}></span>
-          </button>
-        </h2>
-        <div id="accordion-panel-1" className={styles.accordionpanel__close}>
-          <p className={styles.accordionpanel__text}>
-            comma separated valuesファイルです。カンマによって値が分けられているものを指します。
-          </p>
-        </div>
-        <h2 className={styles.accordionheader}>
-          <button className={styles.accordiontrigger} aria-expanded="false" aria-controls="accordion-panel-2">
-          CSVの行だけの差分を調べたい。<span className={styles.accordionicon}></span>
-          </button>
-        </h2>
-        <div id="accordion-panel-2" className={styles.accordionpanel__close}>
-          <p className={styles.accordionpanel__text}>
-            比較したいCSVを二つ選択し、行だけを比較するを押下することで比較することができます。
-          </p>
-        </div>
-        <h2 className={styles.accordionheader}>
-          <button className={styles.accordiontrigger} aria-expanded="false" aria-controls="accordion-panel-3">
-            CSVの値だけの差分を調べたい。<span className={styles.accordionicon}></span>
-          </button>
-        </h2>
-        <div id="accordion-panel-3" className={styles.accordionpanel__close}>
-          <p className={styles.accordionpanel__text}>
-            比較したいCSVを二つ選択し、値だけを比較するを押下することで比較することができます。
-          </p>
-        </div>
-      </div>
-    </div>
+<div>
+  <h1 className={styles.title}>
+    Q&A
+  </h1>
+  <div className={styles.disclosures}>
+    <details className={styles.details}>
+      <summary className={styles.summary}>
+        <h2>CSVとは??<span className={styles.icon}></span></h2>
+      </summary>
+      <p className={styles.text}>
+        comma separated valuesファイルです。カンマによって値が分けられているものを指します。
+      </p>
+    </details>
+    <details className={styles.details}>
+      <summary className={styles.summary}>
+        <h2>CSVの行だけの差分を調べたい。<span className={styles.icon}></span></h2>
+      </summary>
+      <p className={styles.text}>
+        比較したいCSVを二つ選択し、行だけを比較するを押下することで比較することができます。
+      </p>
+    </details>
+    <details className={styles.details}>
+      <summary className={styles.summary}>
+        <h2>CSVの値だけの差分を調べたい。<span className={styles.icon}></span></h2>
+      </summary>
+      <p className={styles.text}>
+        比較したいCSVを二つ選択し、値だけを比較するを押下することで比較することができます。
+      </p>
+    </details>
+    <details className={styles.details}>
+      <summary className={styles.summary}>
+        <h2>比較したCSVの差分をダウンロードしたい。<span className={styles.icon}></span></h2>
+      </summary>
+      <p className={styles.text}>
+        比較を選択した後に、ダウンロードボタンを押すことでCSV出力が可能です。
+      </p>
+    </details>
+  </div>
+</div>
   )
 }
 
