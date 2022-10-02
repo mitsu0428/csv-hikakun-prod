4eec4fef2dbad0f078d9a4fa67c10ba2b0b32057
mitsu0428 <mitsuprograming@gmail.com>
2022-10-03 00:29:58 +0900

    fix(font size)

--- src/pages/components/Accordion.tsx
+++ src/pages/components/Accordion.tsx
@@ -4,13 +4,13 @@ import styles from '../../styles/QuestionAndAnswer.module.css'
 function Accordion() {
   return (
 <div>
-  <h1 className={styles.title}>
+  <h2 className={styles.title}>
     Q&A
-  </h1>
+  </h2>
   <div className={styles.disclosures}>
     <details className={styles.details}>
       <summary className={styles.summary}>
-        <h2>CSVとは??<span className={styles.icon}></span></h2>
+        <h3>CSVとは??<span className={styles.icon}></span></h3>
       </summary>
       <p className={styles.text}>
         comma separated valuesファイルです。カンマによって値が分けられているものを指します。
@@ -18,7 +18,7 @@ function Accordion() {
     </details>
     <details className={styles.details}>
       <summary className={styles.summary}>
-        <h2>CSVの行だけの差分を調べたい。<span className={styles.icon}></span></h2>
+        <h3>CSVの行だけの差分を調べたい。<span className={styles.icon}></span></h3>
       </summary>
       <p className={styles.text}>
         比較したいCSVを二つ選択し、行だけを比較するを押下することで比較することができます。
@@ -26,7 +26,7 @@ function Accordion() {
     </details>
     <details className={styles.details}>
       <summary className={styles.summary}>
-        <h2>CSVの値だけの差分を調べたい。<span className={styles.icon}></span></h2>
+        <h3>CSVの値だけの差分を調べたい。<span className={styles.icon}></span></h3>
       </summary>
       <p className={styles.text}>
         比較したいCSVを二つ選択し、値だけを比較するを押下することで比較することができます。
@@ -34,7 +34,7 @@ function Accordion() {
     </details>
     <details className={styles.details}>
       <summary className={styles.summary}>
-        <h2>比較したCSVの差分をダウンロードしたい。<span className={styles.icon}></span></h2>
+        <h3>比較したCSVの差分をダウンロードしたい。<span className={styles.icon}></span></h3>
       </summary>
       <p className={styles.text}>
         比較を選択した後に、ダウンロードボタンを押すことでCSV出力が可能です。
