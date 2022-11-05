b249cbb71e11f558dc7e0c999afac3831a03e1c2
Mitsuhiro Okada <okada031717@gmail.com>
2022-11-06 01:58:38 +0900

    Revert "feat: microcmsを追加する"

--- src/libs/client.tsx
+++
@@ -1,8 +0,0 @@
-import { createClient } from "microcms-js-sdk"; // CommonJS
-// Initialize Client SDK.
-const client = createClient({
-  serviceDomain: "csvhikakun", // YOUR_DOMAIN is the XXXX part of XXXX.microcms.io
-  apiKey: process.env.API_KEY || "",
-});
-
-export default client;
--- src/pages/404.tsx
+++
@@ -1,8 +0,0 @@
-/* eslint-disable react/react-in-jsx-scope */
-export default function Custom404() {
-  return (
-    <main className="main">
-      <p>ページがありません。</p>
-    </main>
-  );
-}
