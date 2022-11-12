88436f471eca40aa4e5b488de41880be4b97cdd0
mitsu0428 <mitsuprograming@gmail.com>
2022-11-12 22:26:10 +0900

    feat: add sitemap content

--- src/pages/sitemap.xml.tsx
+++ src/pages/sitemap.xml.tsx
@@ -2,24 +2,34 @@ import { GetServerSidePropsContext } from "next";
 
 async function generateSitemapXml() {
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-    <url>
-      <loc>https://csvhikakun.com/</loc>
-      <priority>1.0</priority>
-    </url>
-    <url>
-      <loc>https://csvhikakun.com/about</loc>
-      <priority>0.8</priority>
-    </url>
-    <url>
-      <loc>https://csvhikakun.com/mail</loc>
-      <priority>0.8</priority>
-    </url>
-    <url>
-      <loc>https://csvhikakun.com/privacy</loc>
-      <priority>0.8</priority>
-    </url>
-    </urlset>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+
+  <url>
+    <loc>https://csvhikakun.com/</loc>
+    <priority>1.0</priority>
+  </url>
+  <url>
+    <loc>https://csvhikakun.com/about</loc>
+    <priority>0.8</priority>
+  </url>
+  <url>
+    <loc>https://csvhikakun.com/blog</loc>
+    <priority>0.8</priority>
+  </url>
+  <url>
+    <loc>https://csvhikakun.com/mail</loc>
+    <priority>0.8</priority>
+  </url>
+  <url>
+    <loc>https://csvhikakun.com/privacy</loc>
+    <priority>0.8</priority>
+  </url>
+  <url>
+    <loc>https://csvhikakun.com/blog/article/wvsev_-57gk1</loc>
+    <priority>0.7</priority>
+  </url>
+  
+  </urlset>
     `;
   return xml;
 }
