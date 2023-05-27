8da170965993da06c7493a6872ea1a0c6e4799d7
mitsu0428 <mitsuprograming@gmail.com>
2023-05-27 17:45:47 +0900

    feat: 明日の天気を追加

--- src/pages/whether/index.tsx
+++ src/pages/whether/index.tsx
@@ -17,36 +17,83 @@ const WeatherDescription = styled.p`
 `;
 
 const IndexPage: React.FC = () => {
-  const [weather, setWeather] = useState<any>(null);
+  const [todayWeather, setTodayWeather] = useState<any>(null);
+  const [tomorrowWeather, setTomorrowWeather] = useState<any>(null);
   const city = "Tokyo"; // 取得する都市名
   const apiKey = "9624dd5751a4d41b77bc8d4a92d93cd1"; // OpenWeatherMapのAPIキー
 
   useEffect(() => {
     const fetchWeather = async () => {
       try {
-        const apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&lang=ja&units=metric`;
+        const apiUrl = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&lang=ja&units=metric`;
         const response = await axios.get(apiUrl);
-        setWeather(response.data);
+        const weatherData = response.data.list;
+
+        // 今日の天気情報を取得
+        const today = new Date();
+        const todayWeather = weatherData.find((data: any) => {
+          const dataDate = new Date(data.dt_txt);
+          return (
+            dataDate.getFullYear() === today.getFullYear() &&
+            dataDate.getMonth() === today.getMonth() &&
+            dataDate.getDate() === today.getDate()
+          );
+        });
+
+        // 明日の天気情報を取得
+        const tomorrow = new Date(today);
+        tomorrow.setDate(today.getDate() + 1);
+        const tomorrowWeather = weatherData.find((data: any) => {
+          const dataDate = new Date(data.dt_txt);
+          return (
+            dataDate.getFullYear() === tomorrow.getFullYear() &&
+            dataDate.getMonth() === tomorrow.getMonth() &&
+            dataDate.getDate() === tomorrow.getDate()
+          );
+        });
+
+        setTodayWeather(todayWeather);
+        setTomorrowWeather(tomorrowWeather);
       } catch (error) {
         console.log("Error fetching weather data:", error);
       }
     };
+
     fetchWeather();
   }, []);
 
   return (
     <WeatherContainer>
       <h1>Weather Information</h1>
-      {weather ? (
-        <WeatherInfo>
-          <p>City: {weather.name}</p>
-          <p>Temperature: {weather.main?.temp}°C</p>
-          <WeatherDescription>
-            Description: {weather.weather[0]?.description}
-          </WeatherDescription>
-        </WeatherInfo>
+
+      {todayWeather ? (
+        <>
+          <h2>Weather Today</h2>
+          <WeatherInfo>
+            <p>City: {city}</p>
+            <p>Temperature: {todayWeather.main?.temp}°C</p>
+            <WeatherDescription>
+              Description: {todayWeather.weather[0]?.description}
+            </WeatherDescription>
+          </WeatherInfo>
+        </>
       ) : (
-        <p>Loading...</p>
+        <p>Loading weather... [today]</p>
+      )}
+
+      {tomorrowWeather ? (
+        <>
+          <h2>Weather Tomorrow</h2>
+          <WeatherInfo>
+            <p>City: {city}</p>
+            <p>Temperature: {tomorrowWeather.main?.temp}°C</p>
+            <WeatherDescription>
+              Description: {tomorrowWeather.weather[0]?.description}
+            </WeatherDescription>
+          </WeatherInfo>
+        </>
+      ) : (
+        <p>Loading weather... [tomorrow]</p>
       )}
     </WeatherContainer>
   );
