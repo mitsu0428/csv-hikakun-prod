4329aad9e3d4169346ec99a0a2a0b743cb11dcc9
mitsu0428 <mitsuprograming@gmail.com>
2023-05-30 14:03:07 +0900

    fix: local ストレージの活用

--- src/pages/weather/index.tsx
+++ src/pages/weather/index.tsx
@@ -8,40 +8,55 @@ const IndexPage: React.FC = () => {
   const [todayWeather, setTodayWeather] = useState<any>(null);
   const [tomorrowWeather, setTomorrowWeather] = useState<any>(null);
   const city = "Tokyo"; // 取得する都市名
-  const apiKey = process.env.NEXT_PUBLIC_WEATHER_API_KEY || "";
+  const apiKey =
+    process.env.NEXT_PUBLIC_WEATHER_API_KEY ||
+    "9624dd5751a4d41b77bc8d4a92d93cd1";
 
   useEffect(() => {
     const fetchWeather = async () => {
       try {
-        const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&lang=ja&units=metric`;
-        const response = await axios.get(apiUrl);
-        const weatherData = response.data.list;
+        // ローカルストレージから天気データを取得
+        const storedWeatherData = localStorage.getItem("weatherData");
+        if (storedWeatherData) {
+          const { todayWeather, tomorrowWeather } =
+            JSON.parse(storedWeatherData);
+          setWeatherData(todayWeather, tomorrowWeather);
+        } else {
+          const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&lang=ja&units=metric`;
+          const response = await axios.get(apiUrl);
+          const weatherData = response.data.list;
 
-        // 今日の天気情報を取得
-        const today = new Date();
-        const todayWeather = weatherData.find((data: any) => {
-          const dataDate = new Date(data.dt_txt);
-          return (
-            dataDate.getFullYear() === today.getFullYear() &&
-            dataDate.getMonth() === today.getMonth() &&
-            dataDate.getDate() === today.getDate()
+          // 今日の天気情報を取得
+          const today = new Date();
+          const todayWeather = weatherData.find((data: any) => {
+            const dataDate = new Date(data.dt_txt);
+            return (
+              dataDate.getFullYear() === today.getFullYear() &&
+              dataDate.getMonth() === today.getMonth() &&
+              dataDate.getDate() === today.getDate()
+            );
+          });
+
+          // 明日の天気情報を取得
+          const tomorrow = new Date(today);
+          tomorrow.setDate(today.getDate() + 1);
+          const tomorrowWeather = weatherData.find((data: any) => {
+            const dataDate = new Date(data.dt_txt);
+            return (
+              dataDate.getFullYear() === tomorrow.getFullYear() &&
+              dataDate.getMonth() === tomorrow.getMonth() &&
+              dataDate.getDate() === tomorrow.getDate()
+            );
+          });
+
+          setWeatherData(todayWeather, tomorrowWeather);
+
+          // 天気データをローカルストレージに保存
+          localStorage.setItem(
+            "weatherData",
+            JSON.stringify({ todayWeather, tomorrowWeather })
           );
-        });
-
-        // 明日の天気情報を取得
-        const tomorrow = new Date(today);
-        tomorrow.setDate(today.getDate() + 1);
-        const tomorrowWeather = weatherData.find((data: any) => {
-          const dataDate = new Date(data.dt_txt);
-          return (
-            dataDate.getFullYear() === tomorrow.getFullYear() &&
-            dataDate.getMonth() === tomorrow.getMonth() &&
-            dataDate.getDate() === tomorrow.getDate()
-          );
-        });
-
-        setTodayWeather(todayWeather);
-        setTomorrowWeather(tomorrowWeather);
+        }
       } catch (error) {
         console.log("Error fetching weather data:", error);
       }
@@ -50,6 +65,10 @@ const IndexPage: React.FC = () => {
     fetchWeather();
   }, []);
 
+  const setWeatherData = (todayWeather: any, tomorrowWeather: any) => {
+    setTodayWeather(todayWeather);
+    setTomorrowWeather(tomorrowWeather);
+  };
   return (
     <BasicContainer>
       <SeoSettings
